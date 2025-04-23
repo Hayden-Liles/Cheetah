a35f21ea7e039e30cb940b69ef4317f3e70d644f
Hayden Liles <lileshaydenreal@gmail.com>
2025-04-22 22:17:16 -0600

    Revert "Revert "getting closer""
    This reverts commit a195e3ec48316a6e29cbd751df21fa2efd35c8fc.

--- src/compiler/builtins/print.rs
+++ src/compiler/builtins/print.rs
@@ -75,6 +75,248 @@ impl<'ctx> CompilationContext<'ctx> {
         ).unwrap()
     }
 
+    /// Helper function to print a dictionary value
+    fn print_dict_value(
+        &mut self,
+        dict_ptr: PointerValue<'ctx>,
+        key_type: &Type,
+        value_type: &Type,
+        print_str_fn: inkwell::values::FunctionValue<'ctx>,
+    ) -> Result<(), String> {
+        // 1) Print "{"
+        let open_brace = self.make_cstr("open_brace", b"{\0");
+        let _ = self.builder.build_call(print_str_fn, &[open_brace.into()], "print_open_brace");
+
+        // 2) Get the keys list
+        let dict_keys_fn = self.module.get_function("dict_keys")
+            .ok_or("dict_keys not found".to_string())?;
+        let keys_call = self.builder.build_call(
+            dict_keys_fn,
+            &[dict_ptr.into()],
+            "dict_keys_call",
+        ).unwrap();
+
+        let keys = keys_call
+            .try_as_basic_value().left()
+            .ok_or("Failed to get dictionary keys".to_string())?
+            .into_pointer_value();
+
+        // 3) Get its length
+        let list_len_fn = self.module.get_function("list_len")
+            .ok_or("list_len function not found".to_string())?;
+        let len_call = self.builder.build_call(
+            list_len_fn,
+            &[keys.into()],
+            "keys_len_call",
+        ).unwrap();
+
+        let list_len = len_call
+            .try_as_basic_value().left()
+            .ok_or("Failed to get keys length".to_string())?;
+
+        let list_len_int = list_len.into_int_value();
+
+        // 4) Loop over i in [0..len):
+        // Create basic blocks for the loop
+        let current_function = self.builder.get_insert_block().unwrap().get_parent().unwrap();
+        let loop_cond_block = self.llvm_context.append_basic_block(current_function, "dict_print_cond");
+        let loop_body_block = self.llvm_context.append_basic_block(current_function, "dict_print_body");
+        let loop_exit_block = self.llvm_context.append_basic_block(current_function, "dict_print_exit");
+
+        // Create index variable
+        let index_ptr = self.builder.build_alloca(self.llvm_context.i64_type(), "dict_print_index").unwrap();
+        self.builder.build_store(index_ptr, self.llvm_context.i64_type().const_zero()).unwrap();
+
+        // Jump to loop condition
+        self.builder.build_unconditional_branch(loop_cond_block).unwrap();
+
+        // Loop condition block
+        self.builder.position_at_end(loop_cond_block);
+        let index = self.builder.build_load(self.llvm_context.i64_type(), index_ptr, "dict_index").unwrap();
+        let index_int = index.into_int_value();
+        let cond = self.builder.build_int_compare(
+            inkwell::IntPredicate::SLT,
+            index_int,
+            list_len_int,
+            "dict_index_cond"
+        ).unwrap();
+        self.builder.build_conditional_branch(cond, loop_body_block, loop_exit_block).unwrap();
+
+        // Loop body block
+        self.builder.position_at_end(loop_body_block);
+
+        // Print comma if not the first element
+        let is_first = self.builder.build_int_compare(
+            inkwell::IntPredicate::EQ,
+            index_int,
+            self.llvm_context.i64_type().const_zero(),
+            "is_first_element"
+        ).unwrap();
+
+        let comma_block = self.llvm_context.append_basic_block(current_function, "print_comma");
+        let element_block = self.llvm_context.append_basic_block(current_function, "print_element");
+
+        self.builder.build_conditional_branch(is_first, element_block, comma_block).unwrap();
+
+        // Print comma block
+        self.builder.position_at_end(comma_block);
+        let comma_space = self.make_cstr("comma_space", b", \0");
+        let _ = self.builder.build_call(print_str_fn, &[comma_space.into()], "print_comma_space");
+        self.builder.build_unconditional_branch(element_block).unwrap();
+
+        // Print element block
+        self.builder.position_at_end(element_block);
+
+        // Get list_get function
+        let list_get_fn = self.module.get_function("list_get")
+            .ok_or("list_get function not found".to_string())?;
+
+        // Get dict_get function
+        let dict_get_fn = self.module.get_function("dict_get")
+            .ok_or("dict_get function not found".to_string())?;
+
+        // a) fetch key = list_get(keys, i)
+        let key_call = self.builder.build_call(
+            list_get_fn,
+            &[keys.into(), index_int.into()],
+            "list_get_key_call"
+        ).unwrap();
+
+        let key_val = key_call.try_as_basic_value().left()
+            .ok_or("Failed to get dictionary key".to_string())?;
+
+        // b) inline‐print key (match on key_type)
+        match key_type {
+            Type::Int => {
+                // maybe boxed or direct integer
+                let int_val = if key_val.is_pointer_value() {
+                    let ptr = key_val.into_pointer_value();
+                    self.builder.build_load(self.llvm_context.i64_type(), ptr, "load_int_key")
+                        .unwrap().into_int_value()
+                } else {
+                    key_val.into_int_value()
+                };
+                let print_int_fn = self.module.get_function("print_int")
+                    .ok_or("print_int not found".to_string())?;
+                let _ = self.builder.build_call(print_int_fn, &[int_val.into()], "print_int_key");
+            }
+            Type::Float => {
+                let float_val = if key_val.is_pointer_value() {
+                    let ptr = key_val.into_pointer_value();
+                    self.builder.build_load(self.llvm_context.f64_type(), ptr, "load_float_key")
+                        .unwrap().into_float_value()
+                } else {
+                    key_val.into_float_value()
+                };
+                let print_flt_fn = self.module.get_function("print_float")
+                    .ok_or("print_float not found".to_string())?;
+                let _ = self.builder.build_call(print_flt_fn, &[float_val.into()], "print_float_key");
+            }
+            Type::Bool => {
+                let bool_val = if key_val.is_pointer_value() {
+                    let ptr = key_val.into_pointer_value();
+                    self.builder.build_load(self.llvm_context.bool_type(), ptr, "load_bool_key")
+                        .unwrap().into_int_value()
+                } else {
+                    key_val.into_int_value() // 0 or 1
+                };
+                let print_bool_fn = self.module.get_function("print_bool")
+                    .ok_or("print_bool not found".to_string())?;
+                let _ = self.builder.build_call(print_bool_fn, &[bool_val.into()], "print_bool_key");
+            }
+            Type::String => {
+                let str_ptr = key_val.into_pointer_value();
+                let _ = self.builder.build_call(print_str_fn, &[str_ptr.into()], "print_str_key");
+            }
+            _ => {
+                // fallback placeholder
+                let ph = self.make_cstr("ph_key", format!("<{:?}>\0", key_type).as_bytes());
+                let _ = self.builder.build_call(print_str_fn, &[ph.into()], "print_ph_key");
+            }
+        }
+
+        // c) print ": "
+        let colon_space = self.make_cstr("colon_space", b": \0");
+        let _ = self.builder.build_call(print_str_fn, &[colon_space.into()], "print_colon_space");
+
+        // d) fetch val = dict_get(dict_ptr, key)
+        let val_call = self.builder.build_call(
+            dict_get_fn,
+            &[dict_ptr.into(), key_val.into()],
+            "dict_get_val_call"
+        ).unwrap();
+
+        let val_ptr = val_call.try_as_basic_value().left()
+            .ok_or("Failed to get dictionary value".to_string())?
+            .into_pointer_value();
+
+        // e) inline‐print val (match on value_type)
+        match value_type {
+            Type::Int => {
+                // maybe boxed or direct integer
+                let int_val = self.builder.build_load(self.llvm_context.i64_type(), val_ptr, "load_int_val")
+                    .unwrap().into_int_value();
+                let print_int_fn = self.module.get_function("print_int")
+                    .ok_or("print_int not found".to_string())?;
+                let _ = self.builder.build_call(print_int_fn, &[int_val.into()], "print_int_val");
+            }
+            Type::Float => {
+                let float_val = self.builder.build_load(self.llvm_context.f64_type(), val_ptr, "load_float_val")
+                    .unwrap().into_float_value();
+                let print_flt_fn = self.module.get_function("print_float")
+                    .ok_or("print_float not found".to_string())?;
+                let _ = self.builder.build_call(print_flt_fn, &[float_val.into()], "print_float_val");
+            }
+            Type::Bool => {
+                let bool_val = self.builder.build_load(self.llvm_context.bool_type(), val_ptr, "load_bool_val")
+                    .unwrap().into_int_value();
+                let print_bool_fn = self.module.get_function("print_bool")
+                    .ok_or("print_bool not found".to_string())?;
+                let _ = self.builder.build_call(print_bool_fn, &[bool_val.into()], "print_bool_val");
+            }
+            Type::String => {
+                let _ = self.builder.build_call(print_str_fn, &[val_ptr.into()], "print_str_val");
+            }
+            Type::List(inner) => {
+                self.print_list_value(val_ptr, &*inner, print_str_fn)?;
+            }
+            Type::Dict(_k, _v) => {
+                let print_dict_fn = self.module.get_function("print_dict")
+                    .ok_or("print_dict not found".to_string())?;
+                let _ = self.builder.build_call(print_dict_fn, &[val_ptr.into()], "print_dict_val");
+            }
+            Type::None => {
+                let none_str = self.make_cstr("none_literal_val", b"None\0");
+                let _ = self.builder.build_call(print_str_fn, &[none_str.into()], "print_none_val");
+            }
+            _ => {
+                // fallback placeholder
+                let ph = self.make_cstr("ph_val", format!("<{:?}>\0", value_type).as_bytes());
+                let _ = self.builder.build_call(print_str_fn, &[ph.into()], "print_ph_val");
+            }
+        }
+
+        // Increment index
+        let next_index = self.builder.build_int_add(
+            index_int,
+            self.llvm_context.i64_type().const_int(1, false),
+            "next_index"
+        ).unwrap();
+        self.builder.build_store(index_ptr, next_index).unwrap();
+
+        // Jump back to condition
+        self.builder.build_unconditional_branch(loop_cond_block).unwrap();
+
+        // Exit block
+        self.builder.position_at_end(loop_exit_block);
+
+        // 5) Print "}"
+        let close_brace = self.make_cstr("close_brace", b"}\0");
+        let _ = self.builder.build_call(print_str_fn, &[close_brace.into()], "print_close_brace");
+
+        Ok(())
+    }
+
     /// Helper function to print a list value
     fn print_list_value(
         &mut self,
@@ -292,9 +534,14 @@ impl<'ctx> CompilationContext<'ctx> {
                         print_str_fn,
                     )?;
                 }
-                Type::Dict(_,_) => {
-                    let print_dict_fn = self.module.get_function("print_dict").unwrap();
-                    let _ = self.builder.build_call(print_dict_fn, &[val.into_pointer_value().into()], "print_dict_call");
+                Type::Dict(key_t, value_t) => {
+                    // inline compile‑time dict printing
+                    self.print_dict_value(
+                        val.into_pointer_value(),
+                        &*key_t,
+                        &*value_t,
+                        print_str_fn,
+                    )?;
                 }
                 Type::None => {
                     let none_str = self.make_cstr("none_literal", b"None\0");
