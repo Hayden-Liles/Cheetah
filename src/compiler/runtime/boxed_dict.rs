81021ec4ce431ba32b52a203cc6199c731e0e6bb
Hayden Liles <lileshaydenreal@gmail.com>
2025-04-26 20:38:09 -0600

    Revert "Fixed lists and dicts"
    This reverts commit 5b18e35e0799ba543e5ae2361e3caeac551e4bde.

--- src/compiler/runtime/boxed_dict.rs
+++ src/compiler/runtime/boxed_dict.rs
@@ -162,7 +162,6 @@ fn find_entry(dict: *mut BoxedDict, key: *mut BoxedAny) -> Option<*mut BoxedDict
 
         let entries = (*dict).entries;
         let mut index = (hash % capacity) as usize;
-        let start_index = index;
 
         loop {
             let entry = entries.add(index);
@@ -179,11 +178,6 @@ fn find_entry(dict: *mut BoxedDict, key: *mut BoxedAny) -> Option<*mut BoxedDict
 
             // Linear probing
             index = (index + 1) % capacity as usize;
-
-            // If we've gone all the way around, stop
-            if index == start_index {
-                return None;
-            }
         }
     }
 }
